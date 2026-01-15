9af984ccaa844241016e5df9dcf5b4f2c2c9a1ce
MD10 YWWang1 <YWWANG1@nuvoton.com>
2026-01-15 13:59:52 +0800

    0115A

--- AXI_VIP/rtl/axi_write_slave.sv
+++ AXI_VIP/rtl/axi_write_slave.sv
@@ -1,1 +1,22 @@
-cccc
+module axi_write_slave #(
+	parameter ADDR_WIDTH = 32,
+	parameter DATA_WIDTH = 32
+)(
+	input logic		ACLK;
+	input logic		ARESETn;
+
+	// Write Address Channel
+	input logic [ADDR_WIDTH - 1 : 0] AWADDR,
+	input logic		AWVALID,	
+	output logic	AWREADY,
+
+	// Write Data Channel
+	input logic [DATA_WIDTH - 1 : 0] WDATA,
+	input logic		WVALID,	
+	output logic	WREADY,	
+	
+	// Write Response
+	input logic 	BRESP,
+	input logic		BREADY,
+	output logic	BVALID
+);
