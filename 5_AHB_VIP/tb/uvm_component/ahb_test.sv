da6cc9b0b4ee34f99f69de20cbed2ca7b69f6b05
goddbird <s83011588@gmail.com>
2026-03-17 18:37:33 +0800

    0317D ahb_test TBD

--- 5_AHB_VIP/tb/uvm_component/ahb_test.sv
+++ 5_AHB_VIP/tb/uvm_component/ahb_test.sv
@@ -1,7 +1,7 @@
 class ahb_test extends uvm_test;
     `uvm_component_utils(ahb_test)
     
-    ahb_agent		agent_a;
+    ahb_env		env_a;
 	//ahb_scoreboard
 	
 	function new(string name, uvm_component parent);
@@ -9,15 +9,15 @@ class ahb_test extends uvm_test;
 	endfunction
 	
 	extern function void build_phase(uvm_phase phase);
-	extern function void connect_phase(uvm_phase phase);	
+	extern task run_phase(uvm_phase phase);	
 endclass
 
 function void ahb_test::build_phase(uvm_phase phase);
 	super.build_phase(phase);
-	agent_a	 = ahb_agent::type_id::create("agent_a", this);
+	env_a	 = ahb_env::type_id::create("env_a", this);
 endfunction
 
-function void ahb_test::connect_phase(uvm_phase phase);
+task ahb_test::run_phase(uvm_phase phase);
 	//scoreboard
 	//ahb_agent.monitor_a.ap.connect();
-endfunction
+endtask
