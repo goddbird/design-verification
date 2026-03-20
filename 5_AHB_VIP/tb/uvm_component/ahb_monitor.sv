a932ec18f9c7a40c92aad58c1b4a78565649b962
goddbird <s83011588@gmail.com>
2026-03-20 18:13:14 +0800

    0320D finish coverage

--- 5_AHB_VIP/tb/uvm_component/ahb_monitor.sv
+++ 5_AHB_VIP/tb/uvm_component/ahb_monitor.sv
@@ -1,12 +1,41 @@
 class ahb_monitor extends uvm_monitor;
     `uvm_component_utils(ahb_monitor)
 	
+	covergroup ahb_cov with function sample(ahb_txn  tr, bit [1:0] htrans_val);
+		coverpoint htrans_val {
+			bins idle		= {2'b00};
+			bins busy		= {2'b01};
+			bins nonseq		= {2'b10};
+			bins seq		= {2'b11};
+		}
+
+		coverpoint tr.size {
+			bins byte_8		= {3'b000};
+			bins halfword	= {3'b001};
+			bins word		= {3'b010};
+		}
+
+		coverpoint tr.hburst {
+			bins single		= {3'b000};
+			bins incr		= {3'b001};
+			bins wrap4		= {3'b010};			
+			bins incr4		= {3'b011};
+			bins wrap8		= {3'b100};
+			bins incr8		= {3'b101};
+			bins wrap16		= {3'b110};
+			bins incr16		= {3'b111};			
+		}
+	endgroup
+
+
 	virtual interface ahb_if		vif;
 	uvm_analysis_port #(ahb_txn)	ap;
+
 	
 	function new(string name, uvm_component parent);
 		super.new(name, parent);
 		ap = new("ap", this);
+		ahb_cov = new();
 	endfunction	
 	
 	extern function void build_phase(uvm_phase phase);
@@ -36,7 +65,9 @@ task ahb_monitor::run_phase(uvm_phase phase);
 			@(posedge vif.HCLK);
 
 			tr.data.push_back(vif.HWDATA);
+			ahb_cov.sample(tr, vif.HTRANS);
 			ap.write(tr);
+
 			`uvm_info(get_type_name(), $sformatf("[Monitor] tr.write"), UVM_NONE)
 		end
 	end
