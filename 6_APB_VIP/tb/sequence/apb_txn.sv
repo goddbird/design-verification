53d36ebc815a4008e9147799064f90ef9ac8a37e
goddbird <s83011588@gmail.com>
2026-03-24 16:55:19 +0800

    0324D Ready OOP

--- 6_APB_VIP/tb/sequence/apb_txn.sv
+++ 6_APB_VIP/tb/sequence/apb_txn.sv
@@ -6,6 +6,7 @@ class apb_txn  extends uvm_sequence_item;
 	`uvm_object_utils_begin(apb_txn)
 		`uvm_field_int(addr, UVM_DEFAULT)
 		`uvm_field_int(data, UVM_DEFAULT)
+		`uvm_field_int(is_write, UVM_DEFAULT)
 	`uvm_object_utils_end
 
 	function new(string name = "apb_txn");
