745455d6b918d2f0af79a166137bfd5e7604e709
goddbird <s83011588@gmail.com>
2026-04-22 16:30:14 +0800

    0422D keep adding property

--- 8_Formal_Verification/rtl/fifo_assertion.sv
+++ 8_Formal_Verification/rtl/fifo_assertion.sv
@@ -1,5 +1,25 @@
 // FIFO assertion headfile，可被 include 進任意 module
 
+// reference model
+logic [DATA_WIDTH-1:0] assert_mem [DEPTH];
+logic [4:0] assert_wptr, assert_rptr;
+
+always @(posedge CLK or negedge RESETn) begin
+	if((full && wr) || (empty && rd)) begin
+		// Do nothing, can't write to full or read from empty
+		$display("FIFO full or empty, operation ignored");
+	end
+	else if(wr) begin
+		assert_mem[assert_wptr] <= data_in;
+		assert_wptr <= assert_wptr + 1;
+	end
+	else if(rd) begin
+		data_out <= assert_mem[assert_rptr];
+		assert_rptr <= assert_rptr + 1;
+	end
+end
+
+
 // ================= Formal Assumptions (環境約束) =================
 assume property (@(posedge CLK) disable iff (!RESETn) full  |-> !wr);
 assume property (@(posedge CLK) disable iff (!RESETn) empty |-> !rd);
@@ -29,7 +49,33 @@ endproperty
 assert property (p_fifo_empty_full_consistency)
 	else $error("FIFO empty/full inconsistency");
 
+property p_fifo_reset_status_check;
+	@(posedge CLK)	
+	!RESETn |-> (wptr == 0 && rptr == 0 && !full && empty);
+endproperty
+assert property (p_fifo_reset_status_check);
+	else $error("FIFO reset status incorrect: wptr=%0d, rptr=%0d, full=%b, empty=%b", wptr, rptr, full, empty);
 
+property p_full_empty_check;
+	@(posedge CLK)
+	disable iff (!RESETn)
+	!(full && empty);
+endproperty
+assert property (p_full_empty_check);
+
+property p_data_integrity_out;
+	@(posedge clk) 
+	disable iff (!RESETn)
+	(wr) |-> ##1 (data_out == $past(data_out));
+endproperty
+assert property (p_data_integrity_out);
+
+property p_data_integrity_in;
+	@(posedge clk) 
+	disable iff (!RESETn)
+	(rd) |-> ##1 (data_in == $past(data_in));
+endproperty
+assert property (p_data_integrity_in);
 
 // ================= Cover Properties (驗證狀態可達) =================
 cover property (@(posedge CLK) disable iff (!RESETn) full);
